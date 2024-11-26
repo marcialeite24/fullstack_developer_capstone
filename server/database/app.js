7b4c85a8706739f4b9bf7d6a7d15c5a0510a1295
Marcia Leite <marciasilvaleite@hotmail.com>
2024-11-26 14:47:37 -0500

    Show reviews

--- server/database/app.js
+++ server/database/app.js
@@ -116,8 +116,4 @@ app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
 // Start the Express server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> origin/main
