f29ff80accf3efeedf5632ecb475f02782803287
Marcia Leite <marciasilvaleite@hotmail.com>
2024-11-28 06:53:02 -0500

    Fix lints python

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -155,8 +155,8 @@ def get_inventory(request, dealer_id):
             endpoint = "/carsbymodel/" + str(dealer_id) + "/" + data['model']
         elif 'mileage' in data:
             endpoint = (
-                "/carsbymaxmileage/" 
-                + str(dealer_id) + "/" 
+                "/carsbymaxmileage/"
+                + str(dealer_id) + "/"
                 + data['mileage']
             )
         elif 'price' in data:
